0ce7f5b08c8cf2d3a3693285527000fcf40cb731
hyucheng <v5231965>
2018-08-19 16:11:20 +0800

    提交测试

---
+++ src/router/router.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import index from '@/components/index'
+const _import = require('./_import');
+
+const view = {
+  template: '<router-view></router-view>'
+};
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/test',
+      name: 'index',
+      component: index
+    },
+    {
+      path: '/system',
+      // component: view,
+      children:[
+        {
+          path: '/index',
+          name: '系统管理首页',
+          component: _import('system/index','views')
+        }
+      ]
+    }
+  ]
+})
